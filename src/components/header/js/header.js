1065c497090ccc6931f58c392b1d555e72279abe
timvanschie1 <timvanschie1@gmail.com>
2024-03-22 15:57:05 +0100

    Added header that's hidden on scrolling down and shown when scrolling up

--- src/components/header/js/header.js
+++ src/components/header/js/header.js
@@ -15,18 +15,23 @@ bottomSliceContent.className = 'header-slice__content';
 topSlice.appendChild(topSliceContent);
 bottomSlice.appendChild(bottomSliceContent);
 
-handleScrollOrResize();
+let previousScrollY = null;
+
+handleScrollOrResize(false);
 handleResize();
 
+window.addEventListener('scroll', () => handleScrollOrResize(true));
 window.addEventListener('resize', () => {
-  handleScrollOrResize();
+  handleScrollOrResize(true);
   handleResize();
 });
-window.addEventListener('scroll', handleScrollOrResize);
 
 header.classList.add('header--initialized');
 
-function handleScrollOrResize() {
+function handleScrollOrResize(setPrevScrollPosition) {
+  handleHeaderIsHidden(setPrevScrollPosition);
+  handleHeaderHasBackground();
+
   const headerHeight = header.offsetHeight;
 
   let bgElForTopSlice;
@@ -76,4 +81,55 @@ function setSliceBgClass(sliceEl, bgEl) {
 
 function setSliceHeight(sliceEl, percentage) {
   sliceEl.style.setProperty('--header-slice-height', `${percentage}%`);
+}
+
+function handleHeaderHasBackground() {
+  const { scrollY } = window;
+  const threshold = 100;
+
+  if (scrollY > threshold) {
+    header.classList.add('header--has-background');
+  } else {
+    header.classList.remove('header--has-background');
+  }
+}
+
+/**
+ * Sometimes the scroll event is triggered on page load (when the user didn't scroll).
+ * This causes handleScroll to be executed twice. The setPrevScrollPosition parameter
+ * is a work around to prevent the header from being hidden and directly shown again on page load.
+ **/
+function handleHeaderIsHidden(setPrevScrollPosition) {
+  const { scrollY } = window;
+  const threshold = 100;
+  const isScrolledToTop = scrollY < threshold;
+  const isScrollingDown = scrollY > previousScrollY;
+
+  if (isScrolledToTop) {
+    showHeader();
+  } else if (previousScrollY === null) {
+    hideHeaderWithoutTransition();
+  } else if (isScrollingDown) {
+    hideHeader();
+  } else {
+    showHeader();
+  }
+
+  if (setPrevScrollPosition) {
+    previousScrollY = scrollY;
+  }
+}
+
+function showHeader() {
+  header.classList.remove('header--hidden');
+  header.classList.remove('header--hidden-without-transition');
+}
+
+function hideHeader() {
+  header.classList.add('header--hidden');
+}
+
+function hideHeaderWithoutTransition() {
+  header.classList.add('header--hidden');
+  header.classList.add('header--hidden-without-transition');
 }
